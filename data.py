00e80626b7c9127e3862b8c472c12ba59ba6ff13
Vincitore1402 <vincitoremonster1402@gmail.com>
2018-09-02 18:04:48 +0300

    Initial web-app setup

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article One RL',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, repellendus corrupti enim facere modi odit voluptates saepe temporibus molestiae eos asperiores, dolorem totam ea architecto reiciendis vero, deserunt minus aspernatur.',
+			'author': 'RL',
+			'create_date': '11-14-2017'
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, repellendus corrupti enim facere modi odit voluptates saepe temporibus molestiae eos asperiores, dolorem totam ea architecto reiciendis vero, deserunt minus aspernatur.',
+			'author': 'Kubik',
+			'create_date': '11-14-2017'
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, repellendus corrupti enim facere modi odit voluptates saepe temporibus molestiae eos asperiores, dolorem totam ea architecto reiciendis vero, deserunt minus aspernatur.',
+			'author': 'Olejka',
+			'create_date': '11-14-2017'
+		}
+
+	]
+	return articles
