fe40a05b08cd23ccf407267ce404b9974e9a830e
AndreyKotsenko <51068116+AndreyKotsenko@users.noreply.github.com>
2019-05-28 06:26:19 +0300

    Update static_pages.py

--- routes/static_pages.py
+++ routes/static_pages.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, session, redirect, url_for
 from jinja2 import TemplateNotFound
+
 
 index_page = Blueprint('index_page', __name__,template_folder='templates')
 about_page = Blueprint('about_page', __name__,template_folder='templates')
@@ -8,8 +9,14 @@ about_page = Blueprint('about_page', __name__,template_folder='templates')
 def index():
   try:
     return render_template('home.html')
+
   except TemplateNotFound:
     abort(404)
+
+@index_page.route('/ADMIN')
+def ADMIN():
+  return redirect(url_for('admin.index'))
+
 
 @about_page.route('/about')
 def about():
